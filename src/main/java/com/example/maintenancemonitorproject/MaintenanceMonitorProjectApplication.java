0167aea43cce33772aa4c90322195861a47cbbf1
Jannik-LS <70889142+Jannik-LS@users.noreply.github.com>
2023-05-24 16:26:38 +0200

    Initial commit

---
+++ src/main/java/com/example/maintenancemonitorproject/MaintenanceMonitorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.maintenancemonitorproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonitorProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceMonitorProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maintenancemonitorproject/MaintenanceMonitorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maintenancemonitorproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
