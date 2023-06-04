0f1158ec7c09a2c67c9a7bbb66f49301dd240fbf
Jannik-LS <70889142+Jannik-LS@users.noreply.github.com>
2023-06-04 12:55:02 +0200

    implemented setMessage methode

--- src/main/java/com/example/maintenancemonitorproject/MessageController.java
+++ src/main/java/com/example/maintenancemonitorproject/MessageController.java
@@ -1,19 +1,16 @@
 package com.example.maintenancemonitorproject;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
+    private static Message message = new Message();
 
 
-    @RequestMapping("/monitor/message")
-    public static String sendMessage(){
-        Message message = new Message();
-        if (message.specificMessage.isEmpty()){
-            return message.defaultMessage;
-        } else {
-            return message.specificMessage;
-        }
+    @PostMapping("/monitor/setMessage")
+    public static void setMessage(String newMessage){
+        message.specificMessage = newMessage;
     }
 }
